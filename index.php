8a7b1c318cf117a5d7d9c4586ed376e1019637a8
V@no <git@vano.org>
2022-04-21 03:32:15 -0400

    Initial

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Mine'ish's</title>
-	<link rel="stylesheet" media="screen" href="main.css">
+  <link rel="stylesheet" media="screen" href="main.css">
 </head>
+
 <body>
+  <div class="control">
+    <div>
+      <span>Size:</span>
+      <span>
+        <input id="width" type="number"/>x<input id="height" type="number"/>
+      </span>
+    </div>
+    <div>
+      <span>Bombs:</span>
+      <span>
+        <input id="bombs" type="number"/>
+      </span>
+    </div>
+    <div><span><button id="reset">Reset</button></span></div>
+  </div>
+  <div id="table"></div>
+  <script type="text/javascript" src="main.js"></script>
 </body>
+
 </html>
--- main.js
+++ main.js
@@ -2,5 +2,240 @@
 {
 "use strict";
 
+const elTable = document.getElementById("table"),
+      elWidth = document.getElementById("width"),
+      elHeight = document.getElementById("height"),
+      elBombs = document.getElementById("bombs"),
+      elReset  = document.getElementById("reset"),
+      BOMB = 9,
+      OPEN = 16,
+      FLAG = 32,
+      OFFSET = [-1,-1,0,-1,1,-1,1,0,1,1,0,1,-1,1,-1,0], //offset around current coordinate
+      settings = new Proxy(
+      {
+        width: {default: 10, value: 10, min: 2, max: 1000},
+        height: {default: 10, value: 10, min: 2, max: 1000},
+        bombs: {default: 3, value: 3, min: 1, max: 10000},
+      },
+      {
+        get(target, prop, proxy)
+        {
+          if (prop === "init")
+            return () => this.init(target);
 
+          if (prop == "min" || prop == "max")
+            return Object.keys(target).reduce((obj, key) => (obj[key] = target[key][prop], obj), {});
+
+          return target[prop] && target[prop].value;
+        },
+
+        set(target, prop, value, proxy)
+        {
+          value = this.check(target, prop, value);
+          if (value === undefined)
+            return;
+
+          target[prop].value = value;
+          localStorage.setItem("mineSettings",  JSON.stringify(Object.keys(target).reduce((obj, key) => (obj[key] = target[key].value, obj), {})));
+        },
+
+        init(target)
+        {
+          const data = JSON.parse(localStorage.getItem("mineSettings")) || {};
+          for(let i in target)
+          {
+            const value = this.check(target, i, data[i]);
+            if (value === undefined)
+              continue;
+
+            target[i].value = value;
+          }
+          return target;
+        },
+
+        check(target, prop, value)
+        {
+          let res = prop in target && typeof target[prop].value == typeof value;
+          if (res && "min" in target[prop] && value < target[prop].min)
+            return target[prop].min;
+
+          if (res && "max" in target[prop] && value > target[prop].max)
+            return target[prop].max;
+
+          return res ? value : undefined;
+        }
+      }),
+      table = [];
+
+settings.init();
+init();
+
+[...document.querySelectorAll(".control input")].map(el =>
+{
+  el.value = settings[el.id];
+  el.min = settings.min[el.id];
+  el.max = settings.max[el.id];
+  
+  // el.setAttribute("min", settings.min.width);
+  // el.setAttribute("max", settings.max.width);
+  el.addEventListener("input", e => 
+  {
+    settings[el.id] = +el.value;
+    init();
+  });
+});
+
+elReset.addEventListener("click", init);
+elTable.addEventListener("click", onClick);
+elTable.addEventListener("auxclick", onClick);
+elTable.addEventListener("contextmenu", onClick);
+
+function onClick(e)
+{
+  e.preventDefault();
+  if (e.target === elTable || e.type == "contextmenu")
+    return;
+
+  const index = Array.from(elTable.children).indexOf(e.target),
+        leftClick = e.type == "click";
+
+console.log(e);
+  let val = table[index];
+  if ((leftClick && val & OPEN+FLAG) || (!leftClick && val & OPEN))
+    return console.log("already clicked");
+
+  if (leftClick && (val & 15) == BOMB)
+    return gameover();
+
+  if (leftClick)
+    open(index);
+  else
+  {
+    table[index] = val ^ FLAG;
+    e.target.dataset.type = FLAG;
+  }
+
+  if (!(table[index] & OPEN + FLAG))
+    delete e.target.dataset.type;
+
+//  e.target.textContent = val;
+}
+
+function open(index)
+{
+  let array = [index];
+  while(array.length)
+  {
+    index = array.pop();
+    elTable.children[index].dataset.type = table[index] & 15;
+    table[index] |= OPEN;
+    if (table[index] != OPEN)
+      continue;
+
+    for(let o = 0; o < OFFSET.length; o+=2)
+    {
+      let i = getIndexOffset(index, OFFSET[o], OFFSET[o+1]),
+          val = table[i]; //right
+
+      if (val === undefined || val == BOMB || val & FLAG)
+        continue;
+
+      if (val === 0)
+      {
+        array[array.length] = i;
+        continue;
+      }
+      
+      if (!(val & OPEN))
+      {
+        if (val)
+          elTable.children[i].dataset.type = val;
+
+        table[i] |= OPEN;
+      }
+  
+    }
+  }
+
+}
+
+function gameover()
+{
+  console.log("game over");
+  for(let i = 0; i < table.length; i++)
+  {
+    table[i] |= OPEN;
+    elTable.children[i].dataset.type = table[i] & 15;
+  }
+}
+function rand(min, max)
+{
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function init()
+{
+  table.length = 0; //reset
+  table.length = settings.width * settings.height;
+  const bombs = [],
+        max = Math.min(settings.bombs, table.length - 1);
+
+  while(bombs.length < max)
+  {
+    const bomb = rand(0, table.length-1);
+    if (!bombs.includes(bomb))
+      bombs[bombs.length] = bomb;
+  }
+  for(let i = 0; i < bombs.length; i++)
+    table[bombs[i]] = BOMB;
+
+  while(elTable.children.length > table.length)
+    elTable.removeChild(elTable.lastChild);
+
+  for(let i = 0; i < table.length; i++)
+  {
+    let bombsNum = 0;
+    if (table[i] != BOMB)
+    {
+      for(let o = 0; o < OFFSET.length; o+=2)
+      {
+        let index = getIndexOffset(i, OFFSET[o], OFFSET[o+1]); //right
+// console.log(i, index, bombs);
+        if (table[index] == BOMB)
+          bombsNum++;
+      }
+      table[i] = bombsNum;
+    }
+    const elCell = elTable.children[i] || document.createElement("span");
+    if (!elCell.parentNode)
+      elTable.appendChild(elCell);
+
+    // elCell.textContent = table[i];
+    delete elCell.dataset.type;
+  }
+  elTable.style.setProperty("--cols", settings.width);
+  elTable.style.setProperty("--rows", settings.height);
+  for(let i = 0; i < settings.height; i++)
+  {
+    console.log(table.slice(i * settings.width, i * settings.width + settings.width));
+  }
+  
+  console.log(table);
+  
+}// init();
+
+function indexToXY(index, width = settings.width, height = settings.height)
+{
+  return {x: index % width, y: ~~(index / width)};
+}
+function getIndexOffset(index, offsetX, offsetY, width = settings.width, height = settings.height)
+{
+  let {x, y} = indexToXY(index, width, height);
+  x += offsetX;
+  y += offsetY;
+  if (x < 0 || x > width-1 || y < 0 || y > height-1)
+    return -1;
+
+  return y * width + x;
+}
 }
